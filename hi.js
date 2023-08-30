80beb16aad118e20b0a9c2b6ef4aebc45f9f4a60
naman708 <83646450+naman708@users.noreply.github.com>
2023-08-30 21:47:11 +0530

    getElementByClassName

--- hi.js
+++ hi.js
@@ -1,3 +1,6 @@
-const aI=document.getElementById('addItems');
-aI.style.fontWeight='bold';
-aI.style.color='green';
+const aI=document.getElementsByClassName('list-group-item');
+aI[2].style.backgroundColor='green';
+for(let i=0;i<aI.length;i++)
+{
+    aI[i].style.fontWeight='bold';
+}
