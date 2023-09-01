fda8a60b7ccd6acda62e123a1600229676d90962
naman708 <83646450+naman708@users.noreply.github.com>
2023-09-01 13:39:07 +0530

    filters

--- hi.js
+++ hi.js
@@ -1,15 +1,18 @@
 const form=document.getElementById('addForm');
 const itemList=document.getElementById('items');
 const Input=document.getElementById('item');
+const Input1=document.getElementById('item1');
+const filterI=document.getElementById('filter');
 
 form.addEventListener('submit',addItems);
 itemList.addEventListener('click',delItems);
+filterI.addEventListener('keyup',filterItems);
 
 function addItems(e){
     e.preventDefault();
 
     const li=document.createElement('li');
-    const newItem=document.createTextNode(Input.value);
+    const newItem=document.createTextNode(`${Input.value} ${Input1.value}`);
     const delBtn=document.createElement('button');
     
     delBtn.className='btn btn-danger btn-sm float-right delete';
@@ -26,4 +29,20 @@ function delItems(e){
           itemList.removeChild(li);
         }
       }
+}
+function filterItems(e){
+    var text=e.target.value.toLowerCase();
+
+   var items=itemList.getElementsByTagName('li');
+
+   Array.from(items).forEach(function(i){
+    var itemName=i.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text)!=-1)
+    {
+        i.style.display='block';
+    }
+    else{
+        i.style.display='none';
+    }
+   });
 }
