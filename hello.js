da7986a4e0d94936d63ff67242b3c0f4d93929e0
naman708 <83646450+naman708@users.noreply.github.com>
2023-09-01 19:35:43 +0530

    Lets Scale the app to more users

--- hello.js
+++ hello.js
@@ -4,6 +4,7 @@ const Email=document.getElementById('email');
 const pH=document.getElementById('phno');
 const Date1=document.getElementById('date');
 const Time=document.getElementById('time');
+const userList=document.getElementById('items')
 
 const user={
     Name:`${Fname.value}`,
@@ -17,7 +18,15 @@ Form.addEventListener('submit',storeData);
 
 function storeData(e){
     e.preventDefault();
+
+    const li=document.createElement('li');
+    li.className='list-group-item';
+    li.appendChild(document.createTextNode( `Name:${Fname.value}---Email:${Email.value}
+    PhoneNo:${pH.value}
+    Date:${Date1.value}
+    Time:${Time.value} `))
+    userList.appendChild(li);
 const userJson=JSON.stringify(user);
-localStorage.setItem('UserDetails',userJson);
+localStorage.setItem(`${Email.value}`,userJson);
 const storedUserDetails = JSON.parse(userJson);
 }
